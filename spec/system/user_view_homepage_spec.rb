9468bb03e8128130e56b47a01bbcf0373096fafc
Ronai Melo <amzronai@gmail.com>
2024-04-16 21:22:25 -0300

    Add test for presence of warehouses on home page

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -10,4 +10,23 @@ describe 'User visits homepage' do
     # Assert
     expect(page).to have_content('Galpões & Estoque')
   end
+
+  it 'And sees listed warehouses' do
+    # Arrange
+    Warehouse.create!(name: 'Rio', code:'SDU', city: 'Rio de Janeiro', area: 60000)
+    Warehouse.create!(name: 'Maceio', code:'MCZ', city: 'Maceio', area: 50000)
+    # Act
+    visit('/')
+
+    # Assert
+    expect(page).to have_content('Rio')
+    expect(page).to have_content('SDU')
+    expect(page).to have_content('Rio de Janeiro')
+    expect(page).to have_content('60.000 m²')
+    expect(page).to have_content('Maceio')
+    expect(page).to have_content('MCZ')
+    expect(page).to have_content('Maceio')
+    expect(page).to have_content('50.000 m²')
+
+  end
 end
