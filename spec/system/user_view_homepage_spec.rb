bbd3045f7b4d1777bec1fa3dd39d58d779be0e35
Ronai Melo <amzronai@gmail.com>
2024-04-16 22:25:07 -0300

    Create warehouse Model
    - Warehouse Model was created
    - Migration was run
    - Warehouses variable was added to home#index action
    - Loop over warehouses was added to home index view
    - Message was added to home index view in case there are no warehouses.

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -15,18 +15,29 @@ describe 'User visits homepage' do
     # Arrange
     Warehouse.create!(name: 'Rio', code:'SDU', city: 'Rio de Janeiro', area: 60000)
     Warehouse.create!(name: 'Maceio', code:'MCZ', city: 'Maceio', area: 50000)
+
     # Act
     visit('/')
 
     # Assert
+    expect(page).not_to have_content('Não existem galpões cadastrados')
     expect(page).to have_content('Rio')
     expect(page).to have_content('SDU')
     expect(page).to have_content('Rio de Janeiro')
-    expect(page).to have_content('60.000 m²')
+    expect(page).to have_content('60000 m²')
     expect(page).to have_content('Maceio')
     expect(page).to have_content('MCZ')
     expect(page).to have_content('Maceio')
-    expect(page).to have_content('50.000 m²')
+    expect(page).to have_content('50000 m²')
+  end
 
+  it 'And there are no listed warehouses' do
+    # Arrange
+
+    # Act
+    visit('/')
+
+    # Assert
+    expect(page).to have_content("Não existem galpões cadastrados")
   end
 end
